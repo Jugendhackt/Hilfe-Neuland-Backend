cf22c9630d89d3e4d0338f8ff752e336c27ccd7e
krmax44 <hi@krmax44.de>
2018-11-10 11:36:04 +0100

    implement questionaire serving

--- server.js
+++ server.js
@@ -5,4 +5,21 @@ app.get('/', (req, res) => {
 	res.send('hi!');
 });
 
+const questionaires = {
+	beamer: require('./questionaires/beamer.json')
+};
+
+app.get('/questionaire', (req, res) => {
+	const set = req.query.set;
+	if (Object.keys(questionaires).includes(set)) {
+		res.json(questionaires[set]);
+	}
+	else {
+		res.status(500).json({
+			error: true,
+			errorMsg: 'Questionaire not available'
+		});
+	}
+});
+
 app.listen(3000);

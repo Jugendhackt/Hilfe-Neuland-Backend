348e51e0a0f5ae2b456c9c222ad73c09dc011134
krmax44 <hi@krmax44.de>
2018-11-10 20:02:13 +0100

    implemented symptom algorithm

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const db = require('./connectDb');
 const Symptoms = require('./models/Symptoms');
@@ -10,17 +12,46 @@ app.use((req, res, next) => {
 	next();
 });
 
+app.use(bodyParser.json({ type: 'application/json' }));
+
 app.get('/', (req, res) => {
 	res.send('hi!');
 });
 
-app.get('/starter', async (req, res) => {
+app.get('/starters', async (req, res) => {
 	try {
 		const starterSymptoms = await Symptoms.find({ starter: true });
 		res.json(starterSymptoms);
 	}
 	catch (e) {
 		console.error(e);
+	}
+});
+
+app.post('/symptoms', async (req, res) => {
+	const selectedSymptoms = req.body.symptoms;
+	if (Array.isArray(selectedSymptoms) && selectedSymptoms.length > 0) {
+		const issues = await Issues.find().populate('symptoms');
+		const symptoms = [];
+		const bestIssues = [];
+		issues.forEach(issue => {
+			const applicable = issue.symptoms.some(symptom => selectedSymptoms.includes(symptom.id));
+			if (applicable) {
+				const applicableSymptoms = issue.symptoms.filter(symptom => !selectedSymptoms.includes(symptom.id));
+				applicableSymptoms.forEach(symptom => !symptoms.includes(symptom) ? symptoms.push(symptom) : null);
+			}
+
+			const { _id, text, fix } = issue;
+			bestIssues.push({ _id, text, fix});
+		});
+
+		res.json({ symptoms, bestIssues });
+	}
+	else {
+		res.status(301).json({
+			error: true,
+			errorMsg: 'Invalid data.'
+		});
 	}
 });
 
